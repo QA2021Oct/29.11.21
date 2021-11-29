e1589a612f973f4b35f4e830d806a3d6a4b768a3
QA2021Oct <92875352+QA2021Oct@users.noreply.github.com>
2021-11-29 18:51:01 +0200

    Create HM2.java

---
+++ HM2.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    //Write a method named isEven that accepts an int argument.
+    // The method should return true if the argument is even, or false otherwise.
+
+    static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // true / false
+        System.out.println("Enter a Number - ");
+        int numberFromUser = s.nextInt();
+        System.out.println("is Even? ");
+        boolean result = isEven(numberFromUser);
+        System.out.println(result);
+        //System.out.println(isEven(numberFromUser));  ///same
+    }
+
+    public static boolean isEven(int number){
+        if(number % 2 == 0 ){
+            return true;
+        }
+        else {
+          return false;
+        }
+    }
+}
